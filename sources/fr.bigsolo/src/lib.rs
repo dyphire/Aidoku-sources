f08b1c6425bf2a238740f54e34f6ce6dc489f8fc
apix <apixon@outlook.com>
2025-12-23 22:22:54 +0100

    fix(fr.bigsolo): adapt to new apis (#226)
    * fix(fr.bigsolo): adapt to new apis
    
    * feat(fr.bigsolo): sort catalogue by series title (matches website)
    
    * fix(fr.bigsolo): changes asked by @kkantan
    
    * fix(fr.bigsolo): other changes asked by @kkantan

--- sources/fr.bigsolo/src/lib.rs
+++ sources/fr.bigsolo/src/lib.rs
@@ -4,19 +4,18 @@
 
 #![no_std]
 use aidoku::{
-	AidokuError, AlternateCoverProvider, Chapter, FilterValue, Home, HomeComponent, HomeLayout,
-	Manga, MangaPageResult, MangaStatus, Page, PageContent, Result, Source,
-	alloc::{String, Vec, vec},
+	AlternateCoverProvider, Chapter, DeepLinkHandler, DeepLinkResult, FilterValue, Home,
+	HomeComponent, HomeLayout, Listing, ListingKind, ListingProvider, Manga, MangaPageResult,
+	MangaWithChapter, Page, PageContent, Result, Source,
+	alloc::{String, Vec, string::ToString, vec},
 	imports::{net::Request, std::send_partial_result},
 	prelude::*,
 };
 
 mod models;
-use models::ImageList;
-mod slugify;
-use slugify::slugify;
+use crate::models::{ChapterData, ChapterEndpointData, Series, SeriesList, map_bigsolo_status};
 
-use crate::models::{ConfigJson, SeriesData};
+const BASE_URL: &str = "https://bigsolo.org";
 
 struct BigSolo;
 
@@ -31,70 +30,52 @@ impl Source for BigSolo {
 		_page: i32,
 		_filters: Vec<FilterValue>,
 	) -> Result<MangaPageResult> {
-		let manga_list = BigSolo::get_bigsolo_manga_files();
+		let series_list: SeriesList =
+			Request::get(format!("{BASE_URL}/data/series"))?.json_owned::<SeriesList>()?;
 
 		let mut entries: Vec<Manga> = Vec::new();
 
-		for file_path in manga_list {
-			let response = match Request::get(&file_path).ok().and_then(|r| r.string().ok()) {
-				Some(r) => r,
-				None => {
-					continue; // Skip files that can't be loaded
-				}
-			};
-
-			let series_data: models::SeriesData =
-				match serde_json::from_str::<SeriesData>(&response) {
-					Ok(data) => data,
-					Err(_) => {
-						continue; // Skip files that can't be parsed
-					}
-				};
-
-			// Filter by query if provided
+		// helper function for searching
+		let matches_query = |series: &Series| -> bool {
 			if let Some(ref search_query) = query {
-				let title_lower = series_data.title.to_lowercase();
-				let author_lower = series_data.author.to_lowercase();
-				let artist_lower = series_data.artist.to_lowercase();
+				let title_lower = series.title.to_lowercase();
+				let author_lower = series.author.to_lowercase();
+				let artist_lower = series.artist.to_lowercase();
 				let query_lower = search_query.to_lowercase();
 
-				if !title_lower.contains(&query_lower)
-					&& !author_lower.contains(&query_lower)
-					&& !artist_lower.contains(&query_lower)
-				{
-					continue; // Skip if no match
-				}
+				title_lower.contains(&query_lower)
+					|| author_lower.contains(&query_lower)
+					|| artist_lower.contains(&query_lower)
+					|| series
+						.alternative_titles
+						.iter()
+						.any(|alt_title| alt_title.to_lowercase().contains(&query_lower))
+			} else {
+				true
+			}
+		};
+
+		// process series array
+		for series in series_list.series {
+			if !matches_query(&series) {
+				continue;
 			}
 
-			// convert SeriesData to a minimal Manga entry (to load full data into `get_manga_update`)
-			let title = series_data.title.clone();
-			let slug = slugify(&title);
+			entries.push(series.into());
+		}
 
-			// extract filename from file_path to use as manga entry key
-			let file_name = file_path
-				.split('/')
-				.next_back()
-				.unwrap_or("unknown")
-				.replace(".json", "");
+		// process os array and add One-shot tag
+		for series in series_list.os {
+			if !matches_query(&series) {
+				continue;
+			}
 
-			// add 'One-shot' tag if os is true (for homepage filtering)
-			let mut tags = series_data.tags;
-			if series_data.os.unwrap_or(false) {
+			let mut manga: Manga = series.into();
+			if let Some(tags) = manga.tags.as_mut() {
 				tags.push(String::from("One-shot"));
+			} else {
+				manga.tags = Some(vec![String::from("One-shot")]);
 			}
-
-			let manga = Manga {
-				key: file_name.clone(),
-				title,
-				cover: Some(series_data.cover_low),
-				authors: Some(vec![series_data.author]),
-				artists: Some(vec![series_data.artist]),
-				description: Some(series_data.description),
-				status: BigSolo::map_bigsolo_status(&series_data.release_status),
-				tags: Some(tags),
-				url: Some(format!("https://bigsolo.org/{slug}")),
-				..Default::default()
-			};
 
 			entries.push(manga);
 		}
@@ -111,27 +92,24 @@ impl Source for BigSolo {
 		needs_details: bool,
 		needs_chapters: bool,
 	) -> Result<Manga> {
-		let file_path = format!("https://bigsolo.org/data/series/{}.json", manga.key);
-		let response = Request::get(&file_path)?.string()?;
-		let series_data: SeriesData =
-			serde_json::from_str(&response).map_err(AidokuError::message)?;
+		let api_url = format!("{BASE_URL}/data/series/{}", manga.key);
+		let series_data: Series = Request::get(&api_url)?.json_owned::<Series>()?;
 
 		let title = series_data.title.clone();
-		let slug = slugify(&title);
 
 		if needs_details {
 			manga.title = title;
-			manga.cover = Some(series_data.cover_hq.clone());
+			manga.cover = Some(series_data.cover.url_hq.clone());
 			manga.authors = Some(vec![series_data.author.clone()]);
 			manga.artists = Some(vec![series_data.artist.clone()]);
 			manga.description = Some(series_data.description.clone());
-			manga.status = BigSolo::map_bigsolo_status(&series_data.release_status);
+			manga.status = map_bigsolo_status(&series_data.status);
 			let mut tags = series_data.tags.clone();
 			if series_data.os.unwrap_or(false) {
 				tags.push(String::from("One-shot"));
 			}
 			manga.tags = Some(tags);
-			manga.url = Some(format!("https://bigsolo.org/{slug}"));
+			manga.url = Some(format!("{BASE_URL}/{}", series_data.slug));
 
 			if needs_chapters {
 				send_partial_result(&manga);
@@ -151,29 +129,17 @@ impl Source for BigSolo {
 				chapter_vec
 					.into_iter()
 					.map(|(chapter_key, chapter)| {
-						let chapter_id = chapter
-							.groups
-							.values()
-							.next()
-							.and_then(|url| url.split('/').next_back())
-							.unwrap_or("unknown");
-
+						let chapter_number = chapter_key.parse().ok();
+						let url = format!("{BASE_URL}/{}/{}", series_data.slug, chapter_key);
 						Chapter {
-							key: String::from(chapter_id),
+							key: chapter_key,
 							title: Some(chapter.title),
-							chapter_number: chapter_key.parse().ok(),
-							volume_number: chapter.volume.parse().ok(),
-							date_uploaded: Some(chapter.last_updated.parse().unwrap_or_default()),
-							url: Some(format!("https://bigsolo.org/{slug}/{chapter_key}")),
-							scanlators: {
-								let mut scanlators: Vec<String> =
-									chapter.groups.keys().cloned().collect();
-								if let Some(collab) = chapter.collab.as_ref() {
-									scanlators.push(collab.clone());
-								}
-								Some(scanlators)
-							},
-							locked: chapter.licencied.as_ref().is_some_and(|v| *v),
+							chapter_number,
+							volume_number: chapter.volume.unwrap_or_default().parse().ok(),
+							date_uploaded: Some(chapter.timestamp),
+							url: Some(url),
+							scanlators: Some(chapter.teams),
+							locked: chapter.licensed.unwrap_or(false),
 							..Default::default()
 						}
 					})
@@ -184,61 +150,21 @@ impl Source for BigSolo {
 		Ok(manga)
 	}
 
-	fn get_page_list(&self, _manga: Manga, chapter: Chapter) -> Result<Vec<Page>> {
-		let response = Request::get(format!(
-			"https://bigsolo.org/api/imgchest-chapter-pages?id={}",
-			chapter.key
+	fn get_page_list(&self, manga: Manga, chapter: Chapter) -> Result<Vec<Page>> {
+		let parsed = Request::get(format!(
+			"{BASE_URL}/data/series/{}/{}",
+			manga.key, chapter.key
 		))?
-		.string()?;
-
-		let parsed = serde_json::from_str::<ImageList>(&response).map_err(AidokuError::message)?;
+		.json_owned::<ChapterEndpointData>()?;
 
 		Ok(parsed
+			.images
 			.into_iter()
 			.map(|image| Page {
-				content: PageContent::url(image.link.clone()),
-				has_description: false,
-				thumbnail: Some(image.thumbnail.clone()),
+				content: PageContent::url(image),
 				..Default::default()
 			})
 			.collect())
-	}
-}
-
-impl BigSolo {
-	// gets all manga list of the website
-	fn get_bigsolo_manga_files() -> Vec<String> {
-		let req = match Request::get("https://bigsolo.org/data/config.json")
-			.ok()
-			.and_then(|r| r.string().ok())
-		{
-			Some(r) => r,
-			None => return Vec::new(),
-		};
-
-		let config: ConfigJson = match serde_json::from_str(&req).ok() {
-			Some(cfg) => cfg,
-			None => return Vec::new(),
-		};
-
-		// convert relative paths to absolute urls
-		let urls: Vec<String> = config
-			.LOCAL_SERIES_FILES
-			.into_iter()
-			.map(|file| format!("https://bigsolo.org/data/series/{file}"))
-			.collect();
-
-		urls
-	}
-
-	fn map_bigsolo_status(status: &str) -> MangaStatus {
-		match status {
-			"En cours" => MangaStatus::Ongoing,
-			"Fini" | "Finis" => MangaStatus::Completed,
-			"En pause" => MangaStatus::Hiatus,
-			"Annulé" => MangaStatus::Cancelled,
-			_ => MangaStatus::Unknown,
-		}
 	}
 }
 
@@ -246,31 +172,55 @@ impl Home for BigSolo {
 	fn get_home(&self) -> Result<HomeLayout> {
 		let all_entries = self.get_search_manga_list(None, 1, Vec::new())?.entries;
 
-		// load recommendations
-		let reco_entries = match Request::get("https://bigsolo.org/data/reco.json")
-			.ok()
-			.and_then(|r| r.string().ok())
-		{
-			Some(response) => {
-				match serde_json::from_str::<models::RecoFile>(&response) {
-					Ok(recos) => {
-						// match recommendations to manga data
-						recos
-							.into_iter()
-							.filter_map(|reco| {
-								let file_name = reco.file.replace(".json", "");
-								all_entries
-									.iter()
-									.find(|manga| manga.key == file_name)
-									.cloned()
-							})
-							.collect::<Vec<Manga>>()
-					}
-					Err(_) => Vec::new(),
-				}
-			}
-			None => Vec::new(),
-		};
+		let home_data: SeriesList =
+			Request::get(format!("{BASE_URL}/data/series"))?.json_owned::<SeriesList>()?;
+
+		let reco_entries = home_data
+			.reco
+			.into_iter()
+			.map(Into::into)
+			.collect::<Vec<Manga>>();
+
+		// get the latest chapters from the home data (series and os)
+		let mut latest_chapters: Vec<(String, String, ChapterData)> = home_data
+			.series
+			.into_iter()
+			.chain(home_data.os)
+			.flat_map(|series| {
+				series
+					.chapters
+					.into_iter()
+					.map(move |(chapter_key, chapter)| (series.slug.clone(), chapter_key, chapter))
+			})
+			.collect();
+
+		// Sort by timestamp (most recent first) and take the 10 last
+		latest_chapters.sort_by(|a, b| b.2.timestamp.cmp(&a.2.timestamp));
+		latest_chapters.truncate(10);
+
+		let latest_chapters: Vec<MangaWithChapter> = latest_chapters
+			.into_iter()
+			.filter_map(|(series_slug, chapter_key, chapter_data)| {
+				// Find the manga for this chapter
+				let manga = all_entries.iter().find(|m| m.key == series_slug)?.clone();
+
+				let chapter_number = chapter_key.parse().ok().unwrap_or_default();
+				let url = format!("{BASE_URL}/{}/{}", series_slug, chapter_key);
+				let chapter = Chapter {
+					key: chapter_key,
+					title: Some(chapter_data.title),
+					chapter_number: Some(chapter_number),
+					volume_number: chapter_data.volume.unwrap_or_default().parse().ok(),
+					date_uploaded: Some(chapter_data.timestamp),
+					url: Some(url),
+					scanlators: Some(chapter_data.teams),
+					locked: chapter_data.licensed.unwrap_or(false),
+					..Default::default()
+				};
+
+				Some(MangaWithChapter { manga, chapter })
+			})
+			.collect();
 
 		// filter mangas for the home views (using the 'One-shot' tag)
 		let series_entries: Vec<Manga> = all_entries
@@ -308,6 +258,15 @@ impl Home for BigSolo {
 					},
 				},
 				HomeComponent {
+					title: Some(String::from("Derniers chapitres")),
+					subtitle: None,
+					value: aidoku::HomeComponentValue::MangaChapterList {
+						page_size: None,
+						entries: latest_chapters,
+						listing: None,
+					},
+				},
+				HomeComponent {
 					title: Some(String::from("Séries")),
 					subtitle: None,
 					value: aidoku::HomeComponentValue::Scroller {
@@ -318,32 +277,10 @@ impl Home for BigSolo {
 				HomeComponent {
 					title: Some(String::from("One-shot")),
 					subtitle: None,
-					value: aidoku::HomeComponentValue::MangaList {
-						ranking: false,
-						page_size: None,
+					value: aidoku::HomeComponentValue::Scroller {
 						entries: oneshot_entries.iter().cloned().map(|m| m.into()).collect(),
 						listing: None,
 					},
-				},
-				HomeComponent {
-					title: Some(String::from("Liens")),
-					subtitle: None,
-					value: aidoku::HomeComponentValue::Links(vec![
-						aidoku::Link {
-							title: String::from("Colorisations"),
-							value: Some(aidoku::LinkValue::Url(String::from(
-								"https://bigsolo.org/galerie",
-							))),
-							..Default::default()
-						},
-						aidoku::Link {
-							title: String::from("À propos de Big_herooooo"),
-							value: Some(aidoku::LinkValue::Url(String::from(
-								"https://bigsolo.org/presentation",
-							))),
-							..Default::default()
-						},
-					]),
 				},
 			],
 		})
@@ -352,23 +289,13 @@ impl Home for BigSolo {
 
 impl AlternateCoverProvider for BigSolo {
 	fn get_alternate_covers(&self, manga: Manga) -> Result<Vec<String>> {
-		// reload the manga's json file to get covers_gallery
-		let file_path = format!("https://bigsolo.org/data/series/{}.json", manga.key);
+		let api_url = format!("{BASE_URL}/data/series/{}", manga.key);
 
-		let response = match Request::get(&file_path).ok().and_then(|r| r.string().ok()) {
-			Some(r) => r,
-			None => return Ok(Vec::new()),
-		};
-
-		let series_data: SeriesData = match serde_json::from_str(&response) {
-			Ok(data) => data,
-			Err(_) => return Ok(Vec::new()),
-		};
+		let series_data: Series = Request::get(&api_url)?.json_owned::<Series>()?;
 
 		// extract covers from covers_gallery
 		let covers: Vec<String> = series_data
-			.covers_gallery
-			.unwrap_or_default()
+			.covers
 			.into_iter()
 			.map(|cover| cover.url_hq)
 			.collect();
@@ -377,12 +304,62 @@ impl AlternateCoverProvider for BigSolo {
 	}
 }
 
-// impl DeepLinkHandler for BigSolo {
-//     fn handle_deep_link(&self, _url: String) -> Result<Option<DeepLinkResult>> {
-//         // i'm not even gonna try to handle deep links, it's not worth the effort rn
-//         // the website is getting reworked anyways, i'm not gonna waste my time on this
-//         Ok(None)
-//     }
-// }
+impl DeepLinkHandler for BigSolo {
+	fn handle_deep_link(&self, url: String) -> Result<Option<DeepLinkResult>> {
+		// Strip protocol + domain
+		let path = match url.split(BASE_URL).nth(1) {
+			Some(p) => p.trim_matches('/'),
+			None => return Ok(None),
+		};
 
-register_source!(BigSolo, Home, AlternateCoverProvider);
+		let parts: Vec<&str> = path.split('/').collect();
+
+		match parts.as_slice() {
+			// https://bigsolo.org/series (catalogue)
+			["series"] => Ok(Some(DeepLinkResult::Listing({
+				Listing {
+					id: "catalogue".to_string(),
+					name: "Catalogue".to_string(),
+					kind: ListingKind::Default,
+				}
+			}))),
+
+			// https://bigsolo.org/{series}
+			[series] => Ok(Some(DeepLinkResult::Manga {
+				key: series.to_string(),
+			})),
+
+			// https://bigsolo.org/{series}/{chapter}
+			[series, chapter] => Ok(Some(DeepLinkResult::Chapter {
+				manga_key: series.to_string(),
+				key: chapter.to_string(),
+			})),
+
+			_ => Ok(None),
+		}
+	}
+}
+
+impl ListingProvider for BigSolo {
+	fn get_manga_list(&self, listing: Listing, page: i32) -> Result<MangaPageResult> {
+		if listing.id == "catalogue" {
+			// we need to sort by title because the catalogue is not sorted by title
+			let mut mangas = self.get_search_manga_list(None, page, Vec::new())?.entries;
+			mangas.sort_by(|a, b| a.title.cmp(&b.title));
+			Ok(MangaPageResult {
+				entries: mangas,
+				has_next_page: false,
+			})
+		} else {
+			bail!("Unknown listing")
+		}
+	}
+}
+
+register_source!(
+	BigSolo,
+	Home,
+	AlternateCoverProvider,
+	ListingProvider,
+	DeepLinkHandler
+);
--- sources/fr.bigsolo/src/models.rs
+++ sources/fr.bigsolo/src/models.rs
@@ -1,68 +1,84 @@
 use aidoku::{
-	HashMap,
-	alloc::{String, Vec},
+	HashMap, Manga, MangaStatus,
+	alloc::{String, Vec, format, vec},
 };
 use serde::Deserialize;
 
-#[derive(Default, Deserialize, Debug, Clone)]
-pub struct RecoEntry {
-	pub file: String,
-	// pub color: String,
-}
-
-pub type RecoFile = Vec<RecoEntry>;
-
-#[derive(Default, Deserialize, Debug, Clone)]
-pub struct ImageData {
-	// pub id: String,
-	// pub description: Option<String>,
-	pub link: String,
-	pub thumbnail: String,
-	// pub mp4: i32,
-	// pub position: i32,
-	// pub type: String,
-	// pub size: i64,
-	// pub width: i32,
-	// pub height: i32,
-	// pub nsfw: Option<bool>,
-}
-
-pub type ImageList = Vec<ImageData>;
-
-#[derive(Default, Deserialize, Debug, Clone)]
-#[allow(non_snake_case)]
-pub struct ConfigJson {
-	pub LOCAL_SERIES_FILES: Vec<String>,
-}
-
-#[derive(Default, Deserialize, Debug, Clone)]
-pub struct SeriesData {
-	pub title: String,
-	pub description: String,
-	pub artist: String,
-	pub author: String,
-	pub cover_hq: String,
-	pub cover_low: String,
-	pub tags: Vec<String>,
-	pub release_status: String,
-	pub covers_gallery: Option<Vec<CoverEntry>>,
-	pub chapters: HashMap<String, ChapterData>,
-	pub os: Option<bool>,
-}
-
-#[derive(Default, Deserialize, Debug, Clone)]
-pub struct CoverEntry {
-	pub url_hq: String,
-	// pub url_lq: String,
-	// pub volume: String,
-}
+use crate::BASE_URL;
 
 #[derive(Default, Deserialize, Debug, Clone)]
 pub struct ChapterData {
 	pub title: String,
-	pub volume: String,
-	pub last_updated: String,
-	pub licencied: Option<bool>,
-	pub groups: HashMap<String, String>,
-	pub collab: Option<String>,
+	pub volume: Option<String>,
+	pub timestamp: i64,
+	pub licensed: Option<bool>,
+	pub teams: Vec<String>,
+}
+
+#[derive(Default, Deserialize, Debug, Clone)]
+pub struct ChapterEndpointData {
+	// contains ChapterData and the images.
+	// we're only gonna use the images.
+	pub images: Vec<String>,
+}
+
+#[derive(Default, Deserialize, Debug, Clone)]
+pub struct Cover {
+	pub url_hq: String,
+	pub url_lq: String,
+}
+
+#[derive(Default, Deserialize, Debug, Clone)]
+pub struct Series {
+	pub key: String,
+	pub slug: String,
+	pub title: String,
+	pub description: String,
+	pub artist: String,
+	pub author: String,
+	pub status: String,
+	pub tags: Vec<String>,
+	// pub ja_title: String,
+	pub alternative_titles: Vec<String>,
+	// pub release_rhythm: Option<String>,
+	pub cover: Cover,
+	pub covers: Vec<Cover>,
+	pub os: Option<bool>,
+	pub chapters: HashMap<String, ChapterData>,
+}
+
+#[derive(Default, Deserialize, Debug, Clone)]
+pub struct SeriesList {
+	pub series: Vec<Series>,
+	pub os: Vec<Series>,
+	pub reco: Vec<Series>,
+}
+
+pub fn map_bigsolo_status(status: &str) -> MangaStatus {
+	match status {
+		"En cours" => MangaStatus::Ongoing,
+		"Fini" | "Finis" => MangaStatus::Completed,
+		"En pause" => MangaStatus::Hiatus,
+		"Annulé" => MangaStatus::Cancelled,
+		_ => MangaStatus::Unknown,
+	}
+}
+
+impl From<Series> for Manga {
+	fn from(series: Series) -> Self {
+		let url = Some(format!("{BASE_URL}/{}", series.slug));
+
+		Manga {
+			key: series.slug,
+			title: series.title,
+			cover: Some(series.cover.url_lq),
+			authors: Some(vec![series.author]),
+			artists: Some(vec![series.artist]),
+			description: Some(series.description),
+			status: map_bigsolo_status(&series.status),
+			tags: Some(series.tags),
+			url,
+			..Default::default()
+		}
+	}
 }
