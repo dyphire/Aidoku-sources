c0c5fe68e9166b28750f03b41ed1d656c7d8249e
kantan <31490942+kkantan@users.noreply.github.com>
2026-02-22 15:22:39 -0600

    feat(en.mangademon): home page and listings (#376)

--- sources/en.mangademon/src/lib.rs
+++ sources/en.mangademon/src/lib.rs
@@ -1,10 +1,15 @@
 #![no_std]
 use aidoku::{
-	Chapter, DeepLinkHandler, DeepLinkResult, FilterValue, Manga, MangaPageResult, MangaStatus,
-	Page, PageContent, Result, Source, Viewer,
+	Chapter, DeepLinkHandler, DeepLinkResult, FilterValue, Home, HomeComponent, HomeComponentValue,
+	HomeLayout, HomePartialResult, Listing, ListingProvider, Manga, MangaPageResult, MangaStatus,
+	MangaWithChapter, Page, PageContent, Result, Source, Viewer,
 	alloc::{String, Vec, string::ToString, vec},
 	helpers::uri::{QueryParameters, encode_uri_component},
-	imports::{net::Request, std::parse_date_with_options},
+	imports::{
+		html::Document,
+		net::Request,
+		std::{parse_date_with_options, send_partial_result},
+	},
 	prelude::*,
 };
 
@@ -195,10 +200,164 @@ impl Source for MangaDemon {
 	}
 }
 
+impl Home for MangaDemon {
+	fn get_home(&self) -> Result<HomeLayout> {
+		send_partial_result(&HomePartialResult::Layout(HomeLayout {
+			components: vec![
+				HomeComponent {
+					title: Some("Most Viewed Today".into()),
+					subtitle: None,
+					value: HomeComponentValue::empty_scroller(),
+				},
+				HomeComponent {
+					title: Some("Our Latest Translations".into()),
+					subtitle: None,
+					value: HomeComponentValue::empty_scroller(),
+				},
+				HomeComponent {
+					title: Some("Latest Updates".into()),
+					subtitle: None,
+					value: HomeComponentValue::empty_manga_chapter_list(),
+				},
+				HomeComponent {
+					title: Some("New Titles".into()),
+					subtitle: None,
+					value: HomeComponentValue::empty_scroller(),
+				},
+			],
+		}));
+
+		let html = Request::get(BASE_URL)?.html()?;
+
+		fn parse_scroller(html: &Document, title: &str, listing_id: Option<&str>) {
+			let entries = html
+				.select(format!(
+					".section-title:contains({title}) + .owl-carousel > .owl-element > a"
+				))
+				.map(|els| {
+					els.filter_map(|el| {
+						let url = el.attr("abs:href")?;
+						Some(
+							Manga {
+								key: get_manga_id(&url)?,
+								title: el.attr("title")?,
+								cover: el.select_first("img")?.attr("abs:src"),
+								..Default::default()
+							}
+							.into(),
+						)
+					})
+					.collect()
+				})
+				.unwrap_or_default();
+
+			send_partial_result(&HomePartialResult::Component(HomeComponent {
+				title: Some(title.into()),
+				subtitle: None,
+				value: HomeComponentValue::Scroller {
+					entries,
+					listing: listing_id.map(|id| Listing {
+						id: id.into(),
+						name: title.into(),
+						..Default::default()
+					}),
+				},
+			}));
+		}
+
+		parse_scroller(&html, "Most Viewed Today", None);
+		parse_scroller(
+			&html,
+			"Our Latest Translations",
+			Some("/translationlist.php"),
+		);
+
+		let entries = html
+			.select("#updates-big-container > #updates-container > .updates-element")
+			.map(|els| {
+				els.filter_map(|el| {
+					let link = el.select_first("a")?;
+					let ch_link = el.select_first("a.chplinks")?;
+					Some(MangaWithChapter {
+						manga: Manga {
+							key: get_manga_id(&link.attr("abs:href")?)?,
+							title: link.attr("title")?,
+							cover: el.select_first("img")?.attr("abs:src"),
+							..Default::default()
+						},
+						chapter: Chapter {
+							title: ch_link.text(),
+							// since the date doesn't have time, it's not accurate enough to use
+							// date_uploaded: ch_link
+							// 	.parent()?
+							// 	.next()
+							// 	.and_then(|el| el.text())
+							// 	.and_then(|s| {
+							// 		parse_date_with_options(s, "yyyy-MM-dd", "en_US", "current")
+							// 	}),
+							..Default::default()
+						},
+					})
+				})
+				.collect()
+			})
+			.unwrap_or_default();
+
+		send_partial_result(&HomePartialResult::Component(HomeComponent {
+			title: Some("Latest Updates".into()),
+			subtitle: None,
+			value: aidoku::HomeComponentValue::MangaChapterList {
+				page_size: None,
+				entries,
+				listing: Some(Listing {
+					id: "/lastupdates.php".into(),
+					name: "Latest Updates".into(),
+					..Default::default()
+				}),
+			},
+		}));
+
+		parse_scroller(&html, "New Titles", Some("/newmangalist.php"));
+
+		Ok(HomeLayout::default())
+	}
+}
+
+impl ListingProvider for MangaDemon {
+	fn get_manga_list(&self, listing: Listing, page: i32) -> Result<MangaPageResult> {
+		let html = Request::get(format!("{BASE_URL}{}?list={page}", listing.id))?.html()?;
+
+		let entries: Vec<Manga> = html
+			.select("#updates-container > .updates-element")
+			.map(|els| {
+				els.filter_map(|el| {
+					let img = el.select_first("img")?;
+					let url = el.select_first("a")?.attr("abs:href")?;
+					Some(Manga {
+						key: get_manga_id(&url)?,
+						title: img.attr("title")?,
+						cover: img.attr("abs:src"),
+						url: Some(url),
+						..Default::default()
+					})
+				})
+				.collect()
+			})
+			.unwrap_or_default();
+
+		let has_next_page = !entries.is_empty();
+
+		Ok(MangaPageResult {
+			entries,
+			has_next_page,
+		})
+	}
+}
+
 impl DeepLinkHandler for MangaDemon {
 	fn handle_deep_link(&self, url: String) -> Result<Option<DeepLinkResult>> {
 		Ok(get_manga_id(&url).map(|key| DeepLinkResult::Manga { key }))
 	}
 }
 
-register_source!(MangaDemon, DeepLinkHandler);
+register_source!(MangaDemon, Home, ListingProvider, DeepLinkHandler);
